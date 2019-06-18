24a64c34f8dcb8b3980d754e0012cf1894c0dae3
huangjiang <jiang_huang@ycvr.com>
2019-06-18 18:12:03 +0800

    jenkis 自动部署脚本

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,23 @@
-FROM jenkinsci/blueocean
+FROM centos:7
+
+LABEL maintainer="java runtime environment <522264152@qq.com>"
+
 USER root
-RUN apt-get update \
-        && apt-get install -y sudo
-USER jenkins
-
-MAINTAINER vah/522264152@qq.com
-
-RUN mkdir -p /opt/tools/jdk/
-RUN mkdir -p /opt/tools/maven/
-RUN mkdir -p /opt/tools/git/
 
 
-COPY /usr/lib/jdk/jdk1.8.0_181/ /opt/tools/jdk/jdk_1.8/
-COPY /usr/lib/maven/maven-3.5.4/ /opt/tools/maven/maven_3.5.4/
-COPY /usr/lib/git /opt/tools/git/
+RUN mkdir -p /opt/tools/ \
+    && yum install -y git
 
-# 添加环境变量
-ENV JAVA_HOME /opt/tools/jdk/jdk_1.8
-ENV MAVEN_HOME /opt/tools/maven/maven_3.5.4/
-ENV GIT_HOME /opt/tools/git/
-ENV PATH $PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$GIT_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+COPY jdk1.8.0_181/ /opt/tools/jdk-1.8/
+COPY apache-maven-3.5.4/ /opt/tools/maven-3.5.4/
+COPY apache-tomcat-8.5.42/ /opt/tools/tomcat-8.5/
+
+ENV  JAVA_HOME=/opt/tools/jdk-1.8/ \
+     MAVEN_HOME=/opt/tools/maven-3.5.4/ \
+	 CLASSPATH=$CLASSPATH:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
+
+ENV PATH $PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$MAVEN_HOME/bin
+
+EXPOSE 9090
+
+CMD ["/opt/tools/tomcat-8.5/bin/catalina.sh","run"]
--- 自动发布.sh
+++ 自动发布.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+
 MODULES=`echo $MODULES | sed -r 's/"//g'`
 MODULES=`echo $MODULES | sed -r 's/,/ /g'`
+EVAS=`echo $EVAS | sed -r 's/"//g'`
+EVAS=`echo $EVAS | sed -r 's/,/ /g'`
 echo "开始构建所选模块：${MODULES}"
 VERSION_ARRAY=()
 INDEX1=0
 
 for loop in $MODULES
 do
+	if [ "parent" = ${loop} ]
+    then
+    	mvn clean install -DskipTests -Ddockerfile.skip
+        break
+    fi
 	echo "开始构建: ${loop}模块"
     cd ${loop}
     VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
@@ -20,21 +28,31 @@ done
 
 echo "成功构建所选模块：${MODULES} 即将进行远程部署"
 
-
-INDEX2=0
-for loop in $MODULES
+for eva in $EVAS
 do
-	if [ "base" = ${loop} ]
-    then
-    	INDEX2=$INDEX2+1
-    	continue
-    fi
-    echo "开始部署: ${loop}模块"
+    echo "开始部署: ${eva}环境"
+    INDEX2=0
+    for loop in $MODULES
+    do
+        if [ "parent" = ${loop} ]
+        then
+            break
+        fi
+        if [ "base" = ${loop} ]
+        then
+            INDEX2=$INDEX2+1
+            continue
+        fi
+        echo "开始部署: ${loop}模块"
 
-    scp $loop/target/${loop}"-"${VERSION_ARRAY[${INDEX2}]}".jar" ycvr@192.168.0.140:~/modou/Modou-Backend/${loop}/app
-	ssh ycvr@192.168.0.140 "cd ~/modou/Modou-Backend/; docker-compose rm -sf $loop; docker-compose up -d $loop"
-    INDEX2=$INDEX2+1
-    echo "成功部署: ${loop}模块"
+        scp $loop/target/${loop}"-"${VERSION_ARRAY[${INDEX2}]}".jar" ycvr@192.168.0.${eva}:~/modou/Modou-Backend/${loop}/app
+        ssh ycvr@192.168.0.${eva} "cd ~/modou/Modou-Backend/; docker-compose rm -sf $loop; docker-compose up -d $loop"
+        INDEX2=$INDEX2+1
+        echo "成功部署: ${loop}模块"
+    done
+    echo "完成部署: ${eva}环境"
 done
+
+
 
 echo "成功部署所选模块：${MODULES} ，本次构建任务完成"
