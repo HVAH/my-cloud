d724a33cfd7a2ab488aae6f24003ed9403ff5ca0
huangjiang <huangjiang@codemao.cn>
2020-05-21 23:07:37 +0800

    中心扩展法

--- leetcode/src/main/kotlin/com/vah/leetcode/string/HuiWenStr.kt
+++ leetcode/src/main/kotlin/com/vah/leetcode/string/HuiWenStr.kt
@@ -4,9 +4,39 @@ package com.vah.leetcode.string;
  *@Description
  *@Author HuangJiang
  **/
-class HuiWenStr {
+fun huiwenStr(s: String): String {
+    if (s.isBlank() || s.length == 1) return s
+    var b = 0
+    var end = 0
+    for (i in s.indices) {
+        val len1 = expandLength(s, i, i)
+        val len2 = expandLength(s, i, i + 1)
+        val len = len1.coerceAtLeast(len2)
+        if (len > end - b) {
+            b = i - (len - 1) / 2
+            end = i + len / 2
+        }
+        println("i=$i len1=$len1 len2=$len2 b=$b end=$end")
+    }
+    return s.substring(b, end + 1)
+}
+
+/**
+ * 扩展长度
+ */
+fun expandLength(s: String, l: Int, r: Int): Int {
+    var ll = l
+    var rr = r
+    while (true) {
+        if (ll >= 0 && rr < s.length && s[ll] == s[rr]) {
+            ll--
+            rr++
+        } else {
+            return rr - ll - 1
+        }
+    }
 }
 
 fun main() {
-    println(1111111)
+    println(huiwenStr("aaaaaa"))
 }
